cfea248ab36bd250f153d1822f5edeb09679091f
Cleo Barretto <cleo.web@barretto.com>
2012-10-05 15:33:05 -0700

    Cleanup

---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/MeasurableInfoLogCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  MeasurableInfoLogCollectionView.h
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 9/30/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeasurableInfoLogCollectionView : UICollectionView
+
+@end
---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/MeasurableInfoLogCollectionView.m
@@ -0,0 +1,31 @@
+//
+//  MeasurableInfoLogCollectionView.m
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 9/30/12.
+//
+//
+
+#import "MeasurableInfoLogCollectionView.h"
+
+@implementation MeasurableInfoLogCollectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
