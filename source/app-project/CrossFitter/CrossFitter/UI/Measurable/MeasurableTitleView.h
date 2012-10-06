ac7b313a463067eff9abe87d2fc507fc92272d39
Cleo Barretto <cleo.web@barretto.com>
2012-10-05 20:29:46 -0700

    Adding custom measurable title view

---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/MeasurableTitleView.h
@@ -0,0 +1,16 @@
+//
+//  MeasurableTitleView.h
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 10/5/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeasurableTitleView : UIView
+
+@property IBOutlet UILabel* titleView;
+@property IBOutlet UILabel* subTitleView;
+
+@end
---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/MeasurableTitleView.m
@@ -0,0 +1,13 @@
+//
+//  MeasurableTitleView.m
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 10/5/12.
+//
+//
+
+#import "MeasurableTitleView.h"
+
+@implementation MeasurableTitleView
+
+@end
