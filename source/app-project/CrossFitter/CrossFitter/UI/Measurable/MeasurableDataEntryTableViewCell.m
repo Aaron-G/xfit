fad36f02b4380b9e970f135b4c6c610fad3cecf4
Cleo Barretto <cleo.web@barretto.com>
2012-10-10 08:57:23 -0700

    - Table View cell for measurable data entries on MeasurableLogViewController

---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/MeasurableDataEntryTableViewCell.m
@@ -0,0 +1,17 @@
+//
+//  MeasurableDataEntryTableViewCell.m
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 10/8/12.
+//
+//
+
+#import "MeasurableDataEntryTableViewCell.h"
+
+@implementation MeasurableDataEntryTableViewCell
+
+@synthesize measurableTrendImageButton;
+@synthesize measurableValueLabel;
+@synthesize measurableDateLabel;
+
+@end
