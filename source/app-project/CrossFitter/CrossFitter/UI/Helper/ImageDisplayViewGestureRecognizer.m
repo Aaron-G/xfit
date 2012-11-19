37ad8dbd4f0d580af4c51e284ed2d84fe6f380ba
Cleo Barretto <cleo.web@barretto.com>
2012-11-18 20:08:00 -0800

    Moved to Log folder

---
+++ source/app-project/CrossFitter/CrossFitter/UI/Helper/ImageDisplayViewGestureRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  ImageDisplayViewGestureRecognizer.m
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 11/16/12.
+//
+//
+
+#import "ImageDisplayViewGestureRecognizer.h"
+
+@implementation ImageDisplayViewGestureRecognizer
+
+@end
---
+++ source/app-project/CrossFitter/CrossFitter/UI/Measurable/Log/ImageDisplayViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageDisplayViewController.h
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 11/16/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageDisplayViewController : UIViewController
+
+@end
