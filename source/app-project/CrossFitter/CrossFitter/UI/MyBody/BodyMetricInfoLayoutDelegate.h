7d7cdd5a8c9ab0a711307a7a36f43a137990d584
Cleo Barretto <cleo.web@barretto.com>
2012-12-07 07:58:27 -0800

    Renaming

---
+++ source/app-project/CrossFitter/CrossFitter/UI/MyBody/BodyMetricInfoLayoutDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BodyMetricInfoLayoutDelegate.h
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 11/2/12.
+//
+//
+
+#import "MeasurableInfoLayoutDelegateBase.h"
+
+@interface BodyMetricInfoLayoutDelegate : MeasurableInfoLayoutDelegateBase
+
+@end
---
+++ source/app-project/CrossFitter/CrossFitter/UI/MyBody/BodyMetricInfoLayoutDelegate.m
@@ -0,0 +1,13 @@
+//
+//  BodyMetricInfoLayoutDelegate.m
+//  CrossFitter
+//
+//  Created by Cleo Barretto on 11/2/12.
+//
+//
+
+#import "BodyMetricInfoLayoutDelegate.h"
+
+@implementation BodyMetricInfoLayoutDelegate
+
+@end
